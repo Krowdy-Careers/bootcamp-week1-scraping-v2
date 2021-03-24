06c4f717acf49bea1c626897af5e35d1a3c5d6d3
GlisseLisbeth <glisselisbeth@gmail.com>
2021-03-24 05:08:53 -0500

    update

--- App/scrap.js
+++ App/scrap.js
@@ -1,8 +1,38 @@
-const btnscanprofile = document.getElementById('btnscanprofile');
+const btnscrap = document.getElementById('btnscrap');
 
-btnscanprofile.addEventListener('click', async () => {
+btnscrap.addEventListener('click', async () => {
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     const port = chrome.tabs.connect(tab.id);
     console.log('connected ', port);
-    port.postMessage({acction: 'scanning'});
+    port.postMessage({acction: 'scrapingPagination'})
+    let number = 0
+    let listCandidates = []
+    port.onMessage.addListener(function(response) {
+      if(response.action =="endPagination") {
+        number = parseInt(response?.number)
+        if(number && number>0)
+          port.postMessage({acction:'scrapingListPagination', listCandidates: listCandidates, index:0})
+      }
+      else if(response.action =='endPaginationList'){
+        let {listCandidates, index} = response
+        if(index < number){
+          port.postMessage({acction:'scrapingList', listCandidates: listCandidates,index:index+1})
+        }
+      }
+      else if(response.action =="endListProfile") {
+        listCandidates = response?.listCandidates
+        if(listCandidates&&listCandidates.length>0)
+          port.postMessage({acction:'scrapingProfile', url:listCandidates[0], index:0})
+      }
+      else if(response.action =='endProfile'){
+        const {profile, index} = response
+        if(index < listCandidates.length){
+         listCandidates[index].profile = profile 
+          port.postMessage({acction:'scrapingProfile', url:listCandidates[index+1], index:index+1})
+        }
+        else {
+          port.postMessage({acction:'show candidates'}, listCandidates)
+        }
+      }
+    })
   });
